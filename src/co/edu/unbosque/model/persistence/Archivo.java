acf7a9ebf9dbb4b1663016fd462ea65f6744cb16
Tyreh <oscar7u72@gmail.com>
2021-11-21 00:35:40 -0500

    Added Archivo class to read and write new clients.

--- src/co/edu/unbosque/model/persistence/Archivo.java
+++ src/co/edu/unbosque/model/persistence/Archivo.java
@@ -1,4 +1,48 @@
 package co.edu.unbosque.model.persistence;
 
+import co.edu.unbosque.model.Cliente;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class Archivo {
+
+    private ObjectInputStream entrada;
+
+    private ObjectOutputStream salida;
+
+    public Archivo(File file) {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void escribirArchivo(ArrayList<Cliente> array, File file) {
+        try {
+            salida = new ObjectOutputStream(new FileOutputStream(file));
+            salida.writeObject(array);
+            salida.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public ArrayList<Cliente> leerArchivo(File file) {
+        ArrayList<Cliente> array = new ArrayList<>();
+        if (file.length() != 0) {
+            try {
+                entrada = new ObjectInputStream(new FileInputStream(file));
+                array = (ArrayList<Cliente>) entrada.readObject();
+                entrada.close();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return array;
+    }
 }
